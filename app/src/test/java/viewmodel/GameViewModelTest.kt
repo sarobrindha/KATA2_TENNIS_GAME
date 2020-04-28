74d7269c322daa01abc2c862f84b875d22e38528
Brindha <madhushivam@gmail.com>
2020-04-28 20:25:57 +0200

    Wrote test case for navigation function

--- app/src/test/java/viewmodel/GameViewModelTest.kt
+++ app/src/test/java/viewmodel/GameViewModelTest.kt
@@ -1,6 +1,11 @@
 package viewmodel
 
+import androidx.navigation.NavController
+import com.example.myapplication.R
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
 
 class GameViewModelTest {
 
@@ -9,6 +14,14 @@ class GameViewModelTest {
     @Before
     fun setUp(){
         viewModel = GameViewModel()
+    }
+
+    @Test
+    fun testNavigateToPlayerInfoFragment() {
+        val mockNavController = Mockito.mock(NavController::class.java)
+        val expectedNavController = mockNavController.navigate(R.id.action_startFragment_to_playerInfoFragment)
+        val actualNavController = viewModel.navigateToPlayerInfoFragment(mockNavController, R.id.action_startFragment_to_playerInfoFragment)
+        Assert.assertEquals(expectedNavController,actualNavController)
     }
 
 }
