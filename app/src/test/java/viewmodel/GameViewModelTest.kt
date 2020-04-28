aa65bf5c22e1698d317beb3fe68c91140e426a55
Brindha <madhushivam@gmail.com>
2020-04-28 20:23:37 +0200

    created test class for view model

---
+++ app/src/test/java/viewmodel/GameViewModelTest.kt
@@ -0,0 +1,14 @@
+package viewmodel
+
+import org.junit.Before
+
+class GameViewModelTest {
+
+    private lateinit var viewModel: GameViewModel
+
+    @Before
+    fun setUp(){
+        viewModel = GameViewModel()
+    }
+
+}
